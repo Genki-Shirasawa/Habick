6bfff183a5dd07336326499f33a6ae96e82c2f91
Genki-Shirasawa <bjuuh268@gmail.com>
2021-12-01 19:56:50 +0900

    urls.pyを修正

--- config/urls.py
+++ config/urls.py
@@ -6,10 +6,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('habitapp.urls')),
-] + static(settings.STATIC_URL,
-           document_root=settings.STATICFILES_DIRS)
+]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATICFILES_DIRS)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
 
